9632eefacd6fec35c50f5366c589a96f4f3cb247
Chris Padilla <cpadilla@shoc.us>
2020-08-23 12:26:22 -0500

    Handle file serve with base64 encoding

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const pool = require('./db')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 const fileUpload = require('express-fileupload')
 app.use(cors())
 app.use(fileUpload())
+app.use(bodyParser.json())
 
 var corsOptions = {
   origin: 'http://localhost:3000',
@@ -19,11 +22,46 @@ var corsOptions = {
 app.get('/lessons/:grade', cors(corsOptions), async (req, res) => {
   const { grade } = req.params
   try {
-    const newTest = await pool.query(
+    const lessons = await pool.query(
       `SELECT * FROM lesson WHERE grade = '${grade}'`,
     )
+
     res.setHeader('Content-Type', 'application/json')
-    res.send(newTest.rows)
+    res.send(lessons.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+app.get('/lesson/:id/items', cors(corsOptions), async (req, res) => {
+  const { id } = req.params
+  try {
+    const items = await pool.query(
+      `SELECT ENCODE(file, 'base64') as files FROM item WHERE lesson_id = ${id}`,
+    )
+
+    res.send(items.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+app.post('/lesson', cors(corsOptions), async (req, res) => {
+  const { title, description, grade, email } = req.body
+
+  try {
+    const author = await pool.query(
+      `SELECT * FROM "user" WHERE email = '${email.toLowerCase()}'`,
+    )
+
+    if (author.rows.length > 0) {
+      console.log(req.body)
+      const lesson = await pool.query(`INSERT INTO "lesson" (title, description, grade, author_id)
+      VALUES ('${title}', '${description}', '${grade}', ${author.rows[0].id} ) RETURNING *;`)
+      res.setHeader('Content-Type', 'application/json')
+      res.send(lesson.rows[0].id)
+    }
+
   } catch (err) {
     console.error(err.message)
   }
@@ -31,19 +69,34 @@ app.get('/lessons/:grade', cors(corsOptions), async (req, res) => {
 
 //-----ITEM-------
 app.post('/upload', (req, res) => {
-  if (req.files === null) {
+  const { name, id } = JSON.parse(req.body.jsondata)
+
+  console.log(name, !name)
+  console.log(id, !id)
+  if (req.files === null || !name || !id) {
     return res.status(400).json({ msg: 'No file upload' })
   }
 
   const file = req.files.file
 
-  file.mv(`${__dirname}/uploads/${file.name}`, err => {
+  file.mv(`${__dirname}/uploads/${file.name}`, async err => {
     if (err) {
       console.error(err)
       return res.status(500).send(err)
     }
 
-    res.json({ fileName: file.name, filePath: `uploads/${file.name}` })
+    fs.readFile(`uploads/${file.name}`, async (err, data) => {
+      if (err) {
+        console.error(err)
+      }
+      try {
+        const query = await pool.query(`INSERT INTO item (lesson_id, name, file) VALUES (${id}, '${name}', '\\x${data.toString('hex')}') RETURNING *;`,)
+
+        res.send(query.rows[0].id)
+      } catch (err) {
+        console.log(err)
+      }
+    })
   })
 })
 
